98b5901d1b2d8161294c1042bacddaf2cee53351
igaliva <96495282+igaliva@users.noreply.github.com>
2023-01-04 18:04:39 +0200

    clean up

---
+++ client/src/sandbox/components/Babel.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Babel = () => {
+	return <div>Babel</div>;
+};
+
+export default Babel;
---
+++ client/src/sandbox/components/StringInterpolation.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const StringInterpolation = () => {
+	console.log('in login place in the component');
+	return <div>StringInterpolation</div>;
+};
+
+export default StringInterpolation;
